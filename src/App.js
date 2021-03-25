afb5db669769d42f7fcc3e61303b82cdfb4b17dd
Pitt Wu <kgb00128@gmail.com>
2021-03-25 21:08:48 +0800

    feat：test styled-components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { Button, NewButton } from './constant/Buttons';
+import GlobalStyle from './constant/GlobalStyle';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <div>456</div>
+      <Button className='btn' primary>
+        Click Me
+      </Button>
+      <NewButton>Click Me2</NewButton>
+      <GlobalStyle />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/constant/Theme.js
@@ -0,0 +1,6 @@
+export const theme = {
+  colors: {
+    primary_300: '#333',
+    primary_400: '#444',
+  },
+};
