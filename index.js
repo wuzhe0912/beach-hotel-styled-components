9d420516c7050d6de41e1a91c6c68e9c730ca23e
Pitt Wu <kgb00128@gmail.com>
2021-08-31 14:02:34 +0800

    feat: change sidebar setting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const webdriver = require('selenium-webdriver');
 async function openCrawlerWeb() {
   let driver = await new webdriver.Builder().forBrowser('chrome').build();
 
-  const web = 'https://medium.com/dean-lin';
+  const web = 'https://www.gamer.com.tw/';
 
   driver.get(web);
 }
