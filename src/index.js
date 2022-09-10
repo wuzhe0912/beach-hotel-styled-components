c57df9e1ca90099c3476cad120829efb7f619571
Pitt Wu <kgb00128@gmail.com>
2022-09-10 18:28:45 +0800

    feat: install & use dotenv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+require('dotenv').config();
+
+function getVariable() {
+  const envAppVariable = process.env.APP_VARIABLE;
+  console.log(envAppVariable);
+}
+
+getVariable();
+
 function Hello() {
   console.log('Hello World!');
 }
